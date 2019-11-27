025d17da578c03df2f17808ad0c3b7ce44e07615
rockn1989 <gold_100@bk.ru>
2019-11-27 23:55:06 +0300

    Сборка на Webpack v1

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './js/common.js';
+import './css/main.css';
+import './scss/main.scss';
+// import 'vue';
+// import Vue from 'vue';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+window.Vue = require('vue');
