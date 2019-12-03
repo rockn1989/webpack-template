c92514ce94d466b7bb1853f7d4a1435c59919bbc
Artem <ryabkov@aevrika.ru>
2019-12-03 17:03:08 +0300

    vue init

--- src/index.js
+++ src/index.js
@@ -6,3 +6,15 @@ import './scss/main.scss';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 window.Vue = require('vue');
+import store from './store';
+Vue.component('example-component', require('./components/Example.vue').default);
+
+const app = new Vue({
+  data () {
+    return {
+      component: false
+    }
+  },
+  store,
+  el: '#app'
+});
