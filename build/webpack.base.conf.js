08e6e5b11a8f83dabf350d866a5c969a609d56ce
Artem <ryabkov@aevrika.ru>
2019-12-17 17:43:10 +0300

    Настройка шаблонизатора pug

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -9,6 +10,9 @@ const PATHS = {
 	dist: path.join(__dirname, '../dist'),
 	assets: 'assets/'
 }
+
+const PAGES_DIR = `${PATHS.src}/pug/pages/`;
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith(`.pug`))
 
 module.exports = {
 	externals: {
@@ -40,6 +44,9 @@ module.exports = {
 			test: /\.js$/,
 			loader: 'babel-loader',
 			exclude: '/node_modules/'
+		}, {
+				test: /\.pug$/,
+				loader: 'pug-loader'
 		}, {
 			test: /\.vue$/,
 			loader: 'vue-loader',
@@ -110,7 +117,11 @@ module.exports = {
 			{ from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img`},
 			{ from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts`},
 			{ from: `${PATHS.src}/static`, to: ''},
-		])
+		]),
+		...PAGES.map(page => new HtmlWebpackPlugin({
+			template: `${PAGES_DIR}/${page}`,
+			filename: `./${page.replace(/\.pug/,`.html`)}`
+		}))
 	]
 
 };
